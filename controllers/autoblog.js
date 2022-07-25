f33c9b6f82cb20f844c6a75cc0d26e4335b93d97
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-25 16:37:46 -0700

    New pit function rendering

--- controllers/autoblog.js
+++ controllers/autoblog.js
@@ -2,15 +2,26 @@ const Autoblog = require('../models/autoblog')
 
 module.exports = {
     index, 
-    new: newStyle
+    new: newAutoblog,
+    show,
+    create
 }
 
-function newStyle(req, res) {
-    res.render('autoblog/style');
+function show(req, res) {
+    const auto = new Autoblog(req.body);
+    res.render('autoblog/new', { auto });
+}
+
+function newAutoblog(req, res) {
+    res.render('autoblog/index');
 }
 
 function index(req, res) {
     Autoblog.find({}, function(err, autoblog) {
         res.render('autoblog/index', { autoblog });
     });
+}
+
+function create(req, res) {
+    console.log('create', req.body);
 }
--- routes/autoblog.js
+++ routes/autoblog.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const autoblogCtrl = require('../controllers/autoblog');
+const isLoggedIn = require('../config/auth');
 
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
@@ -8,6 +9,7 @@ const autoblogCtrl = require('../controllers/autoblog');
 // });
 router.get('/', autoblogCtrl.index);
 router.get('/new', autoblogCtrl.new);
-
+router.get('/:id', autoblogCtrl.show);
+router.post('/', autoblogCtrl.create);
 
 module.exports = router;
