6c8b2eeed901eb31b81b2ce45506257f755a2f82
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-26 00:31:42 -0700

    money pit rendering

--- controllers/autoblog.js
+++ controllers/autoblog.js
@@ -7,21 +7,57 @@ module.exports = {
     create
 }
 
-function show(req, res) {
-    const auto = new Autoblog(req.body);
-    res.render('autoblog/new', { auto });
-}
-
-function newAutoblog(req, res) {
-    res.render('autoblog/new');
-}
-
 function index(req, res) {
     Autoblog.find({}, function(err, autoblog) {
-        res.render('autoblog/index', { autoblog });
+      res.render('autoblog/index', { title: 'All Money Pits', autoblog });
     });
-}
+  }
+  
+  function show(req, res) {
+    Autoblog.findById(req.params.id)
+      .exec(function(err, autoblog) {
+            res.render('autoblog/show', {
+              title: 'Autoblog Detail'
+          });
+      });
+  }
+  
+  function newAutoblog(req, res) {
+    res.render('autoblog/new', { title: 'Add Money' });
+  }
+  
+  function create(req, res) {
+    req.body.nowShowing = !!req.body.nowShowing;
+    var autoblog = new Autoblog(req.body);
+    autoblog.save(function(err) {
+      // one way to handle errors
+      if (err) return res.redirect('/autoblog/new');
+      res.redirect(`/autoblog/${autoblog._id}`);
+    });
+  }
+  
 
-function create(req, res) {
-        res.redirect('/autoblog');
-}
+
+
+
+
+
+
+// function show(req, res) {
+//     const auto = new Autoblog(req.body);
+//     res.render('autoblog/new', { auto });
+// }
+
+// function newAutoblog(req, res) {
+//     res.render('autoblog/new');
+// }
+
+// function index(req, res) {
+//     Autoblog.find({}, function(err, autoblog) {
+//         res.render('autoblog/index', { autoblog });
+//     });
+// }
+
+// function create(req, res) {
+//         res.redirect('/autoblog');
+// }
