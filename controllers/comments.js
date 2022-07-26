773b1a2f0a8716a3f492e51adfc2eb2fd453257c
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-26 00:12:53 -0700

    adding comments controller

---
+++ controllers/comments.js
@@ -0,0 +1,35 @@
+const Autoblog = require('../models/autoblog');
+
+module.exports = {
+  create,
+  delete: deleteComment
+};
+
+async function deleteComment(req, res, next) {
+  try {
+    const autoblog = await Autoblog.findOne({'comments._id': req.params.id, 'comments.user': req.user._id});
+    if (!autoblog) throw new Error('Not today!');
+    autoblog.comments.remove(req.params.id);
+    await autoblog.save();
+    res.redirect(`/autoblog/${autoblog._id}`);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+function create(req, res) {
+  // The new review will be embedded in the movie doc
+  Autoblog.findById(req.params.id, function(err, movie) {
+    // Add the user-centric info to req.body
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+
+    autoblog.comments.push(req.body);
+    autoblog.save(function(err) {
+      // Step 5: Data has been changed
+      // so we redirect
+      res.redirect(`/autoblog/${autoblog._id}`);
+    });
+  });
+}
