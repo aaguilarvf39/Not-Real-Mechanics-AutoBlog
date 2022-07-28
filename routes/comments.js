c8e689955abc29d905d3d275792ae4d8bed7ce45
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-27 20:21:50 -0700

    working on ratings

--- routes/comments.js
+++ routes/comments.js
@@ -7,6 +7,5 @@ router.post('/:id', isLoggedIn, commentsCtrl.create);
 router.delete('/:id',isLoggedIn, commentsCtrl.delete);
 router.get('/:id/edit', isLoggedIn, commentsCtrl.edit);
 router.put('/:id', isLoggedIn, commentsCtrl.update);
-// router.put('/:aid/comments/:cid', isLoggedIn, commentsCtrl.update);
 
 module.exports = router;
